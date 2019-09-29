5d2253747a77b58249009dddc35c1be745233f7e
Lazaro <lquin008@gmail.com>
2019-09-29 13:21:35 -0400

    filters added

--- client/src/pages/movie_list/MovieList.page.jsx
+++ client/src/pages/movie_list/MovieList.page.jsx
@@ -1,11 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { useQuery } from "react-apollo";
 import { FetchMovies } from "../../graphql/queries/";
-import { SectionWrapper, List, Loader } from "../../components";
+import { SectionWrapper, List, Loader, Filters } from "../../components";
+import { filterTypes } from "./MovieList.schema";
 
 const MovieList = _ => {
   const [movies, setMovies] = useState([]);
-  const { data, loading } = useQuery(FetchMovies);
+  const [activeFilters, setActiveFilters] = useState({});
+  const { data, loading } = useQuery(FetchMovies, {
+    variables: { where: { ...activeFilters } }
+  });
+
+  const applyFilters = ({genre, ...filtersToApply}) => {
+    const parsedGenres = genre && genre.map(({value}) => value)
+    setActiveFilters({...filtersToApply, ...(parsedGenres && {genre: parsedGenres})});
+
+  }
+
+  const clearFilters = () => {
+    setActiveFilters({})
+  }
 
   useEffect(() => {
     !loading && setMovies(data.movies || []);
@@ -13,6 +27,7 @@ const MovieList = _ => {
 
   return (
     <SectionWrapper>
+      <Filters filterType={filterTypes} onApplyFilters={applyFilters} onClearFilters={clearFilters}/>
       {loading ? <Loader /> : <List movies={movies} />}
     </SectionWrapper>
   );
